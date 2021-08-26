c8a30fc057427853282dc5e6796bb2a736dd9f0b
Artem Sidorenko <artem.sidorenko@t-systems.com>
2021-08-26 16:14:58 +0200

    Refactoring: moving the putty converter
    to the ssh helper
    
    Signed-off-by: Artem Sidorenko <artem.sidorenko@t-systems.com>

--- models/validators.go
+++ models/validators.go
@@ -4,7 +4,6 @@ import (
 	"auri/config"
 	"auri/helpers/ssh"
 	"strconv"
-	"strings"
 
 	"regexp"
 
@@ -68,38 +67,4 @@ func ValidateSSH(sshKey string) error {
 	}
 
 	return nil
-}
-
-//ConvertPuttySSH tries to convert the given ssh key to the OpenSSH authorized_key format
-// returns the key in authorized_key format or error if conversion wasn't possible
-// Conversion is done only on the string basis, there is not cryptographic validation of given key
-func ConvertPuttySSH(sshKey string) (string, error) {
-	header := "---- BEGIN SSH2 PUBLIC KEY ----"
-	footer := "---- END SSH2 PUBLIC KEY ----"
-	commentHeader := "Comment:"
-
-	sshKey = strings.TrimSpace(sshKey)
-	sshKeyLines := strings.Split(sshKey, "\n")
-	if len(sshKeyLines) < 3 || sshKeyLines[0] != header || sshKeyLines[len(sshKeyLines)-1] != footer {
-		return "", errors.New("Invalid putty public key")
-	}
-
-	keyStart := 1
-	keyEnd := len(sshKeyLines) - 1
-	comment := ""
-
-	commentLine := strings.TrimSpace(sshKeyLines[1])
-	if strings.HasPrefix(commentLine, commentHeader) { // found a comment
-		keyStart++
-		comment = strings.ReplaceAll(commentLine, commentHeader, "")
-		comment = strings.TrimSpace(comment)
-		comment = strings.Trim(comment, "\"")
-		comment = strings.Trim(comment, "'")
-	}
-
-	newKey := "ssh-rsa " + strings.Join(sshKeyLines[keyStart:keyEnd], "")
-	if comment != "" {
-		newKey = newKey + " " + comment
-	}
-	return newKey, nil
 }
