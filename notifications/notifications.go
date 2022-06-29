09504c6418e036593e032501efa6d0687b694bd4
Artem Sidorenko <artem.sidorenko@t-systems.com>
2022-06-29 14:17:19 +0200

    Refactoring: transaction guard for admin mails
    to avoid code duplication

--- notifications/notifications.go
+++ notifications/notifications.go
@@ -1,21 +1,12 @@
 package notifications
 
 import (
-	"auri/config"
-	"auri/logger"
 	"auri/mail"
 )
 
 //AdminNotificationNewRequest notifies admins about new request
 func AdminNotificationNewRequest(email, comment string) error {
-	if config.GetInstance().AdminEmailNotificationsEnable {
-		if err := mail.SendAdminNotification(email, comment); err != nil {
-			return err
-		}
-		logger.AuriLogger.Info("Admin notification: admins were successfully notified about new request via email")
-	} else {
-		logger.AuriLogger.Info("Admin notification: email notifications are disabled, skipping notification")
-	}
-
-	return nil
+	return runAdminEmailTransaction("new request from "+email, func() error {
+		return mail.SendAdminNotification(email, comment)
+	})
 }
