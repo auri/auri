c7563d75521ff7765cb2057ff4f8785ad3af4d0d
Artem Sidorenko <artem.sidorenko@t-systems.com>
2022-07-05 15:10:41 +0200

    Feature: run shell hooks for workflow events

--- notifications/helpers.go
+++ notifications/helpers.go
@@ -3,6 +3,13 @@ package notifications
 import (
 	"auri/config"
 	"auri/logger"
+
+	"os"
+	"os/exec"
+	"strings"
+	"unicode"
+
+	"golang.org/x/exp/slices"
 )
 
 //runAdminEmailTransaction runs the given email admin notification transaction
@@ -14,9 +21,57 @@ func runAdminEmailTransaction(name string, f func() error) error {
 	}
 
 	if err := f(); err != nil {
-		return err
+		logger.AuriLogger.Errorf("Admin email notification: %v", err)
+		return nil
 	}
 
 	logger.AuriLogger.Infof("Admin email notification: admins were successfully notified via email about %v", name)
 	return nil
+}
+
+//runAdminShellTransaction runs the given shell commands for admin notification transaction
+//if shell hook notifications are enabled
+func runAdminShellTransaction(
+	name,
+	hook string,
+	hookType string,
+	requestEmail string,
+	requestComment string,
+	adminUser string,
+) error {
+	if !config.GetInstance().AdminShellNotificationsEnable {
+		logger.AuriLogger.Infof("Admin shell notification: shell notifications are disabled, skipping notification for %v", name)
+		return nil
+	}
+
+	if hook == "" {
+		logger.AuriLogger.Errorf("Admin shell notification: shell hook is not defined, skipping notification for %v", name)
+		return nil
+	}
+
+	cmd := exec.Command(hook)
+	cmd.Env = append(os.Environ(),
+		"HOOK_TYPE="+sanitizeShellInput(hookType),
+		"REQUEST_EMAIL="+sanitizeShellInput(requestEmail),
+		"REQUEST_COMMENT="+sanitizeShellInput(requestComment),
+		"ADMIN_USER="+sanitizeShellInput(adminUser),
+	)
+	if err := cmd.Run(); err != nil {
+		logger.AuriLogger.Errorf("Admin shell notification: %v", err)
+		return nil
+	}
+
+	logger.AuriLogger.Infof("Admin shell notification: admins were successfully notified via shell hook about %v", name)
+	return nil
+}
+
+// sanitizeShellInput ensures that provided input is more or less safe to be used in the shellout context
+func sanitizeShellInput(input string) string {
+	allowedChars := []rune{'_', '-', '.', ',', '@', ' '}
+
+	s := strings.FieldsFunc(input, func(r rune) bool {
+		return !unicode.IsLetter(r) && !unicode.IsNumber(r) && !slices.Contains(allowedChars, r)
+	})
+
+	return strings.Join(s, "")
 }
--- notifications/notifications.go
+++ notifications/notifications.go
@@ -1,26 +1,60 @@
 package notifications
 
 import (
+	"auri/config"
 	"auri/mail"
 )
 
 //AdminNotificationNewRequest notifies admins about new request
 func AdminNotificationNewRequest(email, comment string) error {
-	return runAdminEmailTransaction("new request from "+email, func() error {
+	if err := runAdminEmailTransaction("new request from "+email, func() error {
 		return mail.SendAdminNewRequestNotification(email, comment)
-	})
+	}); err != nil {
+		return err
+	}
+
+	return runAdminShellTransaction(
+		"new request from "+email,
+		config.GetInstance().AdminShellNewRequest,
+		"NEW_REQUEST",
+		email,
+		comment,
+		"",
+	)
 }
 
 //AdminNotificationRequestApproved notifies admins about approval of account request
 func AdminNotificationRequestApproved(admin, email, comment string) error {
-	return runAdminEmailTransaction("approval of request from "+email, func() error {
+	if err := runAdminEmailTransaction("approval of request from "+email, func() error {
 		return mail.SendAdminRequestApprovedNotification(admin, email, comment)
-	})
+	}); err != nil {
+		return err
+	}
+
+	return runAdminShellTransaction(
+		"approval of request from "+email,
+		config.GetInstance().AdminShellRequestApproved,
+		"REQUEST_APPROVED",
+		email,
+		comment,
+		admin,
+	)
 }
 
-//AdminNotificationRequestDeclined notifies admins about approval of account request
+//AdminNotificationRequestDeclined notifies admins about rejection of account request
 func AdminNotificationRequestDeclined(admin, email, comment string) error {
-	return runAdminEmailTransaction("rejection of request from "+email, func() error {
+	if err := runAdminEmailTransaction("rejection of request from "+email, func() error {
 		return mail.SendAdminRequestDeclinedNotification(admin, email, comment)
-	})
+	}); err != nil {
+		return err
+	}
+
+	return runAdminShellTransaction(
+		"rejection of request from "+email,
+		config.GetInstance().AdminShellRequestDeclined,
+		"REQUEST_DECLINED",
+		email,
+		comment,
+		admin,
+	)
 }
